c596e6b93797b1f632273be5f58c96953068cfbe
ryan <2869888186@qq.com>
2019-03-25 12:03:20 +0800

    第四个版本

--- carousel/carousel-1/js/main.js
+++ carousel/carousel-1/js/main.js
@@ -1,13 +1,53 @@
-var $allButtons = $('#buttons>button')
+var $allButtons=$('#buttons>button')
 //通过jQuery把符合CSS选择器（#buttons>span）的元素筛选出来,并封装成一个jQuery对象，取名为$allButtons。$allButtons是一个伪数组
 console.log($allButtons)
-for (let i = 0; i < $allButtons.length; i++) {//$allButtons是一个伪数组,遍历它
-    $($allButtons).on('click', function (x) {       //$($allButtons)的onclick事件
-        var index = $(x.currentTarget).index()      //index()从匹配的元素中搜索给定元素的索引值，从0开始计数
-        var n = index * -300
-        $('#images').css({
-            transform: 'translateX(' + n + 'px)'    //实现左移300像素
-        })
+for(let i=0;i<$allButtons.length;i++){
+  $($allButtons).on('click',function(x){
+    var index=$(x.currentTarget).index()
+    var p=index*-300
+    $('#images').css({
+      transform:'translateX('+p+'px)'
     })
+    n=index
+    $allButtons.eq(n)
+    .addClass('red')
+    .siblings('.red').removeClass('red')
+  })
 }
+
+let n=0
+let size=$allButtons.length
+let  timeId=setInterval(()=>{
+
+  $allButtons.eq(n%size).trigger('click')
+  .addClass('red')
+  .siblings('.red').removeClass('red')
+  n+=1
+},1000)
+
+$('.window').on('mouseenter',function(){
+  window.clearInterval(timeId)
+})
+
+$('.window').on('mouseleave',function(){
+  timeId=setInterval(()=>{
+
+    $allButtons.eq(n%size).trigger('click')
+    .addClass('red')
+    .siblings('.red').removeClass('red')
+    n+=1
+  },1000)
+})
+
+
+
+
+
+
+
+
+
+
+
+
 
