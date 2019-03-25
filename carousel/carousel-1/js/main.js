dc62248fcca521cd306a4af4fb934c0d6136c57f
ryan <2869888186@qq.com>
2019-03-25 12:31:32 +0800

    第五个版本

--- carousel/carousel-1/js/main.js
+++ carousel/carousel-1/js/main.js
@@ -1,53 +1,41 @@
-var $allButtons=$('#buttons>button')
+var $allButtons = $('#buttons>button')
 //通过jQuery把符合CSS选择器（#buttons>span）的元素筛选出来,并封装成一个jQuery对象，取名为$allButtons。$allButtons是一个伪数组
-console.log($allButtons)
-for(let i=0;i<$allButtons.length;i++){
-  $($allButtons).on('click',function(x){
-    var index=$(x.currentTarget).index()
-    var p=index*-300
-    $('#images').css({
-      transform:'translateX('+p+'px)'
+
+let n = 0
+let size = $allButtons.length
+let timeId = setTimer()
+
+// 执行过程
+for (let i = 0; i < $allButtons.length; i++) {
+    $($allButtons).on('click', function (x) {
+        var index = $(x.currentTarget).index()
+        var p = index * -300
+        $('#images').css({
+            transform: 'translateX(' + p + 'px)'
+        })
+        n = index
+        activeButton($allButtons.eq(n))
     })
-    n=index
-    $allButtons.eq(n)
-    .addClass('red')
-    .siblings('.red').removeClass('red')
-  })
 }
-
-let n=0
-let size=$allButtons.length
-let  timeId=setInterval(()=>{
-
-  $allButtons.eq(n%size).trigger('click')
-  .addClass('red')
-  .siblings('.red').removeClass('red')
-  n+=1
-},1000)
-
-$('.window').on('mouseenter',function(){
-  window.clearInterval(timeId)
+$('.window').on('mouseenter', function () {
+    window.clearInterval(timeId)
 })
-
-$('.window').on('mouseleave',function(){
-  timeId=setInterval(()=>{
-
-    $allButtons.eq(n%size).trigger('click')
-    .addClass('red')
-    .siblings('.red').removeClass('red')
-    n+=1
-  },1000)
+$('.window').on('mouseleave', function () {
+    setTimer()
 })
 
 
-
-
-
-
-
-
-
-
-
-
-
+// activeButton($Button)，playSlide(index)，setTimer()函数
+function activeButton($Button) {
+    $Button.addClass('red')
+        .siblings('.red').removeClass('red')
+}
+function playSlide(index) {
+    $allButtons.eq(index).trigger('click')
+}
+function setTimer() {
+    return setInterval(() => {
+        playSlide(n % size)
+        n += 1
+    }, 1000)
+}
