4af4e30a8141403495f71be93d7390f0ebf6777e
ryan <2869888186@qq.com>
2019-03-25 11:10:15 +0800

    第三个版本

--- carousel/carousel-1/js/main.js
+++ carousel/carousel-1/js/main.js
@@ -1,20 +1,13 @@
-$(p1).on('click', function () {
-    $(images).css({
-        transform:'translateX(0)'
+var $allButtons = $('#buttons>button')
+//通过jQuery把符合CSS选择器（#buttons>span）的元素筛选出来,并封装成一个jQuery对象，取名为$allButtons。$allButtons是一个伪数组
+console.log($allButtons)
+for (let i = 0; i < $allButtons.length; i++) {//$allButtons是一个伪数组,遍历它
+    $($allButtons).on('click', function (x) {       //$($allButtons)的onclick事件
+        var index = $(x.currentTarget).index()      //index()从匹配的元素中搜索给定元素的索引值，从0开始计数
+        var n = index * -300
+        $('#images').css({
+            transform: 'translateX(' + n + 'px)'    //实现左移300像素
+        })
     })
-})
-$(p2).on('click', function () {
-    $(images).css({
-        transform:'translateX(-300px)'
-    })
-})
-$(p3).on('click', function () {
-    $(images).css({
-        transform:'translateX(-600px)'
-    })
-})
-$(p4).on('click', function () {
-    $(images).css({
-        transform:'translateX(-900px)'
-    })
-})
+}
+
