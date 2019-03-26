901dc077640a26b59661de4101bc482a09e62fd7
ryan <2869888186@qq.com>
2019-03-26 19:14:41 +0800

    无缝轮播第二个版本

--- carousel/carousel-2/js/main.js
+++ carousel/carousel-2/js/main.js
@@ -1,42 +1,193 @@
-let n
-初始化()
-setInterval(() => {
-    makeLeave(getImage(n))
-        .one('transitionend', (e) => {
-            makeEnter($(e.currentTarget))
-        });
+let $buttons = $('#buttonWrapper>button')
+let $slides = $('#slides')
+let $images = $slides.children('img')
+let current = 0
 
-    makeCurrent(getImage(n + 1))
-    n += 1
-}, 1000)
+makeFakeSlides()
+buttonEvents()
 
-
-//初始化
-function 初始化() {
-    n = 1
-    $(`.images>img:child(${n})`).addClass('current')
-        .siblings().addClass('enter')
+//控制上下页切换
+$(next).on('click',function () {
+    goToSlide(current+1)
+})
+$(previous).on('click',function () {
+    goToSlide(current-1)
+})
+//控制自动播放
+let timer =setInterval(function () {
+    goToSlide(current+1)
+},1500)
+$('.container').on('mouseenter',function () {
+    window.clearInterval(timer)
+}).on('mouseleave',function () {
+    timer =setInterval(function () {
+        goToSlide(current+1)
+    },1500)
+})
+//展示假的首页
+function makeFakeSlides(){
+    let $firstCopy = $images.eq(0).clone(true)
+    let $lastCopy = $images.eq($images.length - 1).clone(true)
+    $slides.append($firstCopy)
+    $slides.prepend($lastCopy)
+    $slides.css({transform: 'translateX(-300px)'})
 }
-//工具函数
-function x(n) {
-    if (n > 4) {
-        n = n % 4
-        if (n === 0) {
-            n = 4
-        }
+//切换到应该到
+function goToSlide(index){
+    if (index>$buttons.length-1){
+        index=0
+    }else if(index<0){
+        index=$buttons.length-1
     }
-    return n;
+    if(current===$buttons.length-1 && index===0) {
+        //最后一张到第一张
+        console.log("这是从最后一张到copy第一张")
+        $slides.css({transform: `translateX(${-(current+2)*300}px)`})
+            .one('transitionend', function () {
+                $slides.hide().offset()
+                $slides.css({transform:`translateX(${-(index+1)*300}px)`}).show()
+                //.css({display:"flex"})
+                console.log("更换copy第一张为第一张")
+            })
+    }else if(current===0 && index===$buttons.length-1){
+        //第一张到最后一张
+        console.log("这是从第一张到copy最后一张")
+        $slides.css({transform: 'translateX(0px)'})
+            .one('transitionend', function () {
+                $slides.hide().offset()
+                $slides.css({transform:`translateX(${-(index+1)*300}px)`}).show()
+                console.log("更换copy最后一张为第一张")
+            })
+    }else{
+        $slides.css({transform:`translateX(${-(index+1)*300}px)`})
+    }
+    current = index
 }
-function getImage(n) {
-    return $(`.images > img:nth-child(${x(n)})`)
+function buttonEvents() {
+    $('#buttonWrapper').on('click','button',function (e) {
+        let $button=$(e.currentTarget)
+        let index=$button.index()
+        goToSlide(index)
+    })
 }
-//状态切换
-function makeCurrent($node) {
-    return $node.removeClass('enter leave').addClass('current')
-}
-function makeLeave($node) {
-    return $node.removeClass('current enter').addClass('leave')
-}
-function makeEnter($node) {
-    return $node.removeClass('leave enter').addClass('enter')
-}
+    // $buttons.eq(0).on('click', function () {
+    //     if (current === 3) {
+    //         console.log("这是从第4张到第1张")
+    //         $slides.css({transform: 'translateX(-1500px)'})
+    //             .one('transitionend', function () {
+    //                 $slides.hide()
+    //                     .offset()
+    //                 $slides.css({transform: 'translateX(-300px)'})
+    //                     .show()
+    //
+    //             })
+    //         // $slides.css({transform:"translateX(-1600px)"})
+    //     } else {
+    //         $slides.css({
+    //             transform: "translateX(-300px)"
+    //         })
+    //     }
+    //     current = 0
+    //     console.log(current)
+    //
+    // })
+    // $buttons.eq(1).on('click', function () {
+    //     $slides.css({
+    //         transform: "translateX(-600px)"
+    //     })
+    //     current = 1
+    //     console.log(current)
+    // })
+    // $buttons.eq(2).on('click', function () {
+    //     $slides.css({
+    //         transform: "translateX(-900px)"
+    //     })
+    //     current = 2
+    //     console.log(current)
+    // })
+    // $buttons.eq(3).on('click', function () {
+    //     if (current === 0) {
+    //         console.log("这是从第1张到第4张")
+    //         $slides.css({transform: 'translateX(0)'})
+    //             .one('transitionend', function () {
+    //                 $slides.hide()
+    //                     .offset()
+    //                 $slides.css({transform: 'translateX(-1200px)'})
+    //                     .show()
+    //                 console.log("zhege")
+    //             })
+    //     } else {
+    //         $slides.css({
+    //             transform: "translateX(-1200px)"
+    //         })
+    //     }
+    //     current = 3
+    //     console.log(current)
+    // })
+
+
+
+
+
+// $($buttons[0]).on('click',function () {
+//     if (current===3){
+//         console.log("这是从第4张到第1张")
+//         // $slides.css({transform:"translateX(-1600px)"})
+//     }
+//     $slides.css({
+//         transform:"translateX(-300px)"
+//     })
+//     current =0
+//     console.log(current)
+// })
+// $($buttons[1]).on('click',function () {
+//     $slides.css({
+//         transform:"translateX(-600px)"
+//     })
+//     current =1
+//     console.log(current)
+// })
+// $($buttons[2]).on('click',function () {
+//     $slides.css({
+//         transform:"translateX(-900px)"
+//     })
+//     current =2
+//     console.log(current)
+// })
+// $($buttons[3]).on('click',function () {
+//     if (current===0){
+//         console.log("这是从第1张到第4张")
+//     }
+//     $slides.css({
+//         transform:"translateX(-1200px)"
+//     })
+//     current =3
+//     console.log(current)
+// })
+
+// let $buttons = $('#buttonsWrapper > button')
+// let $slides =$('#slides')
+// console.log($slides)
+
+
+// $buttons.eq(0).on('click',function(){
+//     $slides.css({
+//         transform:'translateX(0px)'
+//     })
+// })
+
+// $buttons.eq(1).on('click',function(){
+//     $slides.css({
+//         transform:'translateX(-300px)'
+//     })
+// })
+// $buttons.eq(2).on('click',function(){
+//     $slides.css({
+//         transform:'translateX(-600px)'
+//     })
+// })
+// $buttons.eq(3).on('click',function(){
+//     $slides.css({
+//         transform:'translateX(-900px)'
+//     })
+// })
--- carousel/carousel-2/readme.md
+++ carousel/carousel-2/readme.md
@@ -1,11 +1,19 @@
 这是无缝轮播的初步实现方案<br>
+
 主要有以下功能：<br>
 实现的思想是：<br>
 欢迎优化和改进<br>
 
+
+[预览链接](https://douglasryan.github.io/demo/carousel/carousel-2)
 第一个版本：<br>
 使用css建立状态集合（用户可以用css自己定义样式）
 使用js切换状态（使用jQuery库）
 本状态集合为——横向平移
 
-[预览链接](https://douglasryan.github.io/demo/carousel/carousel-2)
+
+第二个版本：<br>
+使用flex横向布局，使用transform改变位置实现轮播，<br>
+复制最后一张图片到第一张前面，复制第一张图片到最后一张后面，监听最后一张和第一张改变，在改变结束后用正确的图片替换复制的图片。<br>
+用按钮click事件绑定改变位置的功能，<br>
+监听图片的onmouseenter事件和onmouseleave事件，实现悬停在图片上，停止轮播。<br>
